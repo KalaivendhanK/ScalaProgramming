f4876bfdccefbc5a8fb4befa1519fc56bb590047
kalaivendhanK <kalaisundaram93@gmail.com>
2019-08-18 11:30:25 +0530

    Modified source and tests for Set using Classes

--- src/main/scala/com/home/collections/SetC.scala
+++ src/main/scala/com/home/collections/SetC.scala
@@ -1,9 +1,55 @@
 package com.home.collections
 
 trait SetC extends (String => Boolean) {
-
+  def add(input: String): SetC
+  def remove(input: String): SetC
+  def size(currentSize: Int): Int
+  def empty = SetC.Empty
 }
 
 object SetC {
+  case object Empty extends SetC {
+    override def apply(input: String): Boolean =
+      false
 
+    final override def add(input: String): SetC =
+      NonEmpty(input, this)
+
+    final override def size(currentSize: Int = 0) = 0
+
+    def remove(input: String): SetC =
+      this
+  }
+
+  /*private[this]*/ case class NonEmpty(element: String, otherElements: SetC) extends SetC {
+    override def apply(input: String): Boolean =
+      input == element || otherElements(input)
+
+    final override def add(input: String): SetC =
+      if (input == element || input == otherElements(element))
+        this
+      else
+        NonEmpty(input, otherElements.add(element))
+
+    final override def size(currentSize: Int = 0): Int = {
+      var elementCount = 0
+      if (otherElements.isInstanceOf[Empty.type])
+        elementCount = currentSize + 1
+      else if (otherElements.isInstanceOf[NonEmpty]) {
+        elementCount = currentSize + 1
+        elementCount = otherElements.size(currentSize = elementCount)
+      }
+      elementCount
+    }
+
+    def remove(input: String): SetC =
+      if (input == element)
+        otherElements
+      else
+        NonEmpty(element, otherElements.remove(input))
+
+  }
+  final def empty: SetC = Empty
+  final def add(input: String): SetC =
+    NonEmpty(input, SetC.Empty)
 }
--- src/test/scala/collections/SetCTestSuite.scala
+++ src/test/scala/collections/SetCTestSuite.scala
@@ -4,10 +4,81 @@ import org.scalatest.{ FunSuite, Matchers }
 import com.home.collections.SetC
 
 class SetCTestSuite extends FunSuite with Matchers {
+  private def set = SetC
   /**
     * Tests to check the empty set funcionality
     */
   test("Checking an elemnt in an empty set should return false") {
+    set.empty shouldBe a[SetC]
+  }
+
+  /**
+    * Test to check the add functionality on emptysets
+    */
+  test("adding an element on an empty set should produce a NonEmpty set") {
+    val first = randomString
+    val second = randomString
+    set.empty.add(first) shouldBe a[SetC]
+    set.empty.add(first) shouldBe a[SetC.NonEmpty]
+
+    val setWithOneElement = set.empty.add(first)
+    setWithOneElement(first) shouldBe true
+    setWithOneElement(second) shouldBe false
+
+    val setWithTwoElements = set.empty.add(first).add(second)
+    setWithTwoElements(first) shouldBe true
+    setWithTwoElements(second) shouldBe true
+  }
+
+  /**
+    * Test to check the remove functionality on emptysets
+    */
+  test("remove on an empty set should produce an Empty set") {
+    val first = randomString
+    val second = randomString
+    val third = randomString
+    val fourth = randomString
+
+    set.empty.remove(first).size(0) shouldBe 0
+
+    val removedOneElement = set.empty.add(first).remove(first)
+    removedOneElement(first) shouldBe false
+    removedOneElement(second) shouldBe false
+
+    val removedTwoElements = set.empty.add(first).add(second).remove(first)
+    removedTwoElements(first) shouldBe false
+    removedTwoElements(second) shouldBe true
+
+    val setToCheckTheNullElements = set.add(first).add(second).add(second).add(third).add(fourth)
+    // setToCheckTheNullElements(null) shouldBe false
+    setToCheckTheNullElements.size(0) shouldBe 4
+    println(setToCheckTheNullElements.size(0))
 
   }
+  /**
+    * Test to check the duplicate handling in SetC
+    */
+  test("Duplicate elements should not exists in Sets") {
+    val first = randomString
+    val second = randomString
+
+    val setWithDuplicates = set.empty.add(first).add(first).add(second)
+    setWithDuplicates.size(0) shouldBe 2
+  }
+
+  test("calling empty on a NonEmpty set should yield an empty Set") {
+    val first = randomString
+    val second = randomString
+
+    val nonEmptySet = set.add(first).add(second)
+    val nonEmptyTurnedEmpty = nonEmptySet.empty
+
+    nonEmptySet(first) shouldBe true
+    nonEmptySet(second) shouldBe true
+    nonEmptyTurnedEmpty(first) shouldBe false
+    nonEmptyTurnedEmpty(second) shouldBe false
+    nonEmptyTurnedEmpty.size(0) shouldBe 0
+  }
+
+  def randomString: String = scala.util.Random.alphanumeric.take(5).mkString
 }
